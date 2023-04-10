51d10137fa90e5b52d19800ec01ac0be4180a99c
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-04-10 17:51:51 +0200

    deps: install prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,11 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className=''>
+    <main className="">
       <div className="flex items-center justify-center h-screen">
-        <h1 className='text-white '>Hi I&apos;m Nathan</h1>
+        <h1 className="text-white text-3xl font-extrabold">
+          Hi I&apos;m Nathan
+        </h1>
       </div>
     </main>
   )
