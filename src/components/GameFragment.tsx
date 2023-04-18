3aa85d3fd0a34e32a32ac5e083a0916abeecc36b
NathanDjianMartin <ndmroot-git@yahoo.fr>
2023-04-18 20:03:56 +0200

    feat: improve GameFragment

--- src/components/GameFragment.tsx
+++ src/components/GameFragment.tsx
@@ -1,32 +1,39 @@
+import Stack from '@/components/Stack'
 import Heading from '@/components/typography/Heading'
 import { UserPlusIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import React from 'react'
 
 export default function GameFragment() {
-  const paragraphStyleClasses = 'text-white text-base mb-6'
+  const imageStyleClasses = 'mb-6'
+  const paragraphStyleClasses = 'text-white text-base px-4'
   return (
-    <div className="flex flex-col items-center bg-neutral-900 px-4">
-      <Heading size="xlarge" className="text-center mt-12">
+    <div className='flex flex-col items-center bg-neutral-900'>
+      <Heading size="xlarge" className="text-center">
         It all started with a video game.
       </Heading>
-      <Image
-        src="/minecraft.webp"
-        alt="minecraft logo"
-        width={300}
-        height={100}
-      ></Image>
       <p className={paragraphStyleClasses}>
         In Minecraft you can write Java plugins to improve your players
         experience.
       </p>
+      <Image
+        src="/minecraft.webp"
+        alt="minecraft logo"
+        width={500}
+        height={1000}
+        className={imageStyleClasses}
+      ></Image>
       <p className={paragraphStyleClasses}>
-        At the <b>age of 14</b>, I was running my own minecraft server and wrote
-        <b> my first Java plugin</b>.
+        At the <b>age of 14</b> I wrote
+        <b> my first Java plugin</b> from scratch.
       </p>
-      <p className={paragraphStyleClasses}>
-        That&apos;s when I fell in love with computer science 😍.
-      </p>
+      <Image
+        src="/nathan-minecraft.webp"
+        alt="young nathan playing minecraft"
+        width={500}
+        height={1000}
+        className={imageStyleClasses}
+      ></Image>      
     </div>
   )
 }
